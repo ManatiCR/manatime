dd51488058e53b394d830a1354ac73a4d6d9174b
rohernandez <roberthb94@gmail.com>
2016-09-19 21:14:16 -0600

    Created the content_type and template to show the timesheet.

--- modules/features/manatime_feature_timesheets/themes/timesheet-view.tpl.php
+++ modules/features/manatime_feature_timesheets/themes/timesheet-view.tpl.php
@@ -5,23 +5,35 @@
  * Template file for timesheet.
  */
  ?>
- <aside class="time-entries-block">
-   <ul class="time-entries-list">
-     <?php foreach ($records as $record): ?>
-       <li>
-         <p class="record--project"><?php print $record['project']?></p>
-         <p class="record--task"><?php print $record['task']?></p>
-           <ul class="record--entries">
-             <?php foreach ($record['entries'] as $entry): ?>
-               <li>
-                 <?php print $entry['date']; ?>
-                 <p title="<?php print $entry['description']; ?>">
-                   <?php print $entry['duration']; ?>
-                 </p>
-               </li>
-             <?php endforeach; ?>
-           </ul>
-       </li>
-     <?php endforeach; ?>
-   </ul>
+<aside class="time-entries-block">
+  <table class="time-entries-list">
+    <tr>
+      <th>Description</th>
+      <th>Sunday</th>
+      <th>Monday</th>
+      <th>Tuesday</th>
+      <th>Wednesday</th>
+      <th>Thursday</th>
+      <th>Friday</th>
+      <th>Saturday</th>
+    </tr>
+    <?php foreach ($records as $record): ?>
+      <tr class="record--entries">
+        <th>
+          <p class="record--project"><?php print $record['project']?></p>
+          <p class="record--task"><?php print $record['task']?></p>
+        </th>
+        <!-- 7: Days of the week. -->
+        <?php for ($number_day = 0; $number_day < 7; $number_day++): ?>
+          <?php if (isset($record['entries'][$number_day])): ?>
+            <th title="<?php print $record['entries'][$number_day][0]['description']; ?>">
+              <?php print $record['entries'][$number_day][0]['duration']; ?>
+            </th>
+          <?php else: ?>
+            <th><?php print '<a title="add time entry" href="#">add</a>'; ?></th>
+          <?php endif; ?>
+        <?php endfor; ?>
+      </tr>
+    <?php endforeach; ?>
+  </table>
 </aside>
