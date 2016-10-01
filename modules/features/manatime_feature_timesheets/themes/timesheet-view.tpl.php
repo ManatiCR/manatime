76dab66fab48a0ad91a43f68f364ab96b225f5fc
rohernandez <roberthb94@gmail.com>
2016-10-01 14:00:12 -0600

    Applied little changes.

--- modules/features/manatime_feature_timesheets/themes/timesheet-view.tpl.php
+++ modules/features/manatime_feature_timesheets/themes/timesheet-view.tpl.php
@@ -8,39 +8,50 @@
 
  <main id="time-entries-main">
    <?php if (empty($records)): ?>
-     <p>empty</p>
+     <p>Empty.</p>
    <?php else: ?>
        <table class="time-time_entries-list">
-         <tr>
-           <th>Description</th>
-           <th>Sunday</th>
-           <th>Monday</th>
-           <th>Tuesday</th>
-           <th>Wednesday</th>
-           <th>Thursday</th>
-           <th>Friday</th>
-           <th>Saturday</th>
-         </tr>
-         <?php foreach ($records as $record): ?>
-           <tr class="record--time-entries">
-             <th>
-               <p class="record--project"><?php print $record['project']?></p>
-               <p class="record--task"><?php print $record['task']?></p>
-             </th>
-             <!-- 7: Days of the week. -->
-             <?php for ($number_day = 0; $number_day < 7; $number_day++): ?>
-               <?php if (isset($record['time_entries'][$number_day])): ?>
-                 <th>
-                   <p title="<?php print $record['time_entries'][$number_day][0]['description']; ?>">
-                     <?php print $record['time_entries'][$number_day][0]['duration']; ?>
-                   </p>
-                 </th>
-               <?php else: ?>
-                 <th><?php print '<a title="add time entry" href="#">add</a>'; ?></th>
-               <?php endif; ?>
-             <?php endfor; ?>
+         <thead>
+           <tr>
+             <th><?php print t('Task'); ?></th>
+             <th><?php print t('Monday'); ?></th>
+             <th><?php print t('Tuesday'); ?></th>
+             <th><?php print t('Wednesday'); ?></th>
+             <th><?php print t('Monday'); ?></th>
+             <th><?php print t('Friday'); ?></th>
+             <th><?php print t('Saturday'); ?></th>
+             <th><?php print t('Sunday'); ?></th>
+             <th><?php print t('Total'); ?></th>
            </tr>
-         <?php endforeach; ?>
+         </thead>
+         <tbody>
+           <?php foreach ($records as $record): ?>
+             <tr class="record--time-entries">
+               <th>
+                 <p class="record--project"><?php print $record['project']?></p>
+                 <p class="record--task"><?php print $record['task']?></p>
+               </th>
+               <!-- 7: Days of the week. -->
+               <?php for ($number_day = 1; $number_day <= 7; $number_day++): ?>
+                 <?php if (isset($record['time_entries'][$number_day])): ?>
+                   <th>
+                     <p title="<?php print $record['time_entries'][$number_day][0]['description']; ?>">
+                       <?php print $record['time_entries'][$number_day][0]['duration']; ?>
+                     </p>
+                   </th>
+                 <?php else: ?>
+                   <th><?php print '<a title="Add time entry" href="#">add</a>'; ?></th>
+                 <?php endif; ?>
+               <?php endfor; ?>
+               <th>
+                 <?php print $record['total_hours_worked']; ?>
+               </th>
+               <th>
+                 <?php print '<a href="timesheet/remove-row-form/' . $id_timesheet . '/' . $record['task_id'] . '/nojs" class="ctools-use-modal">' . t('Remove row') . '</a>';?>
+               </th>
+             </tr>
+           <?php endforeach; ?>
+         <tbody>
        </table>
      <?php endif; ?>
    <?php print $add_row_link; ?>
