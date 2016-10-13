2fb7adf9998b8adfb081e2cfacc6b72a236505b6
rohernandez <roberthb94@gmail.com>
2016-10-12 18:39:35 -0600

    Added a function to edit time entries.

--- modules/features/manatime_feature_timesheets/themes/timesheet-view.tpl.php
+++ modules/features/manatime_feature_timesheets/themes/timesheet-view.tpl.php
@@ -13,38 +13,23 @@
        <table class="time-time_entries-list">
          <thead>
            <tr>
-             <th><?php print t('Task'); ?></th>
-             <th><?php print t('Monday'); ?></th>
-             <th><?php print t('Tuesday'); ?></th>
-             <th><?php print t('Wednesday'); ?></th>
-             <th><?php print t('Monday'); ?></th>
-             <th><?php print t('Friday'); ?></th>
-             <th><?php print t('Saturday'); ?></th>
-             <th><?php print t('Sunday'); ?></th>
+             <th><?php print t('Description'); ?></th>
+             <?php foreach ($records[0]['time_entries'] as $key => $record): ?>
+               <th><?php print $key; ?></th>
+             <?php endforeach; ?>
              <th><?php print t('Total'); ?></th>
            </tr>
          </thead>
          <tbody>
-           <?php foreach ($records as $key => $record): ?>
+           <?php foreach ($records as $record): ?>
              <tr class="record--time-entries">
                <th>
                  <p class="record--project"><?php print $record['project']?></p>
                  <p class="record--task"><?php print $record['task']?></p>
                </th>
-               <!-- 7: Days of the week. -->
-               <?php for ($number_day = 1; $number_day <= 7; $number_day++): ?>
-                 <?php if (isset($record['time_entries'][$number_day])): ?>
-                   <th>
-                     <p title="<?php print $record['time_entries'][$number_day][0]['description']; ?>">
-                       <?php print $record['time_entries'][$number_day][0]['duration']; ?>
-                     </p>
-                   </th>
-                 <?php else: ?>
-                   <th>
-                     <?php print '<a href="timesheet/add-time-entry-form/' . $timesheet_id . '/' . $record['project_id'] . '/' . $record['task_id'] . '/' . $number_day . '/nojs" class="ctools-use-modal">' . t('Add') . '</a>';?>
-                   </th>
-                 <?php endif; ?>
-               <?php endfor; ?>
+               <?php foreach ($record['time_entries'] as $entry): ?>
+                 <th><?php print $entry; ?></th>
+               <?php endforeach; ?>
                <th>
                  <?php print $record['total_hours_worked']; ?>
                </th>
