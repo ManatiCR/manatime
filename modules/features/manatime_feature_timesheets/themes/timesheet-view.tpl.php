79ac546725c1a97904f22273cf416671f62259db
rohernandez <roberthb94@gmail.com>
2016-10-19 17:32:05 -0600

    Improved the way the timesheet days names are gotten.

--- modules/features/manatime_feature_timesheets/themes/timesheet-view.tpl.php
+++ modules/features/manatime_feature_timesheets/themes/timesheet-view.tpl.php
@@ -7,39 +7,37 @@
  ?>
 
  <main id="time-entries-main">
-   <?php if (empty($records)): ?>
-     <p>Empty.</p>
-   <?php else: ?>
-       <table class="time-time_entries-list">
-         <thead>
-           <tr>
-             <th><?php print t('Description'); ?></th>
-             <?php foreach ($records[0]['time_entries'] as $key => $record): ?>
-               <th><?php print $key; ?></th>
+   <table class="time-time_entries-list">
+     <thead>
+       <tr>
+         <th><?php print t('Description'); ?></th>
+         <?php foreach (array_keys($timesheet_days) as $dayname): ?>
+           <th><?php print $dayname; ?></th>
+         <?php endforeach; ?>
+         <th><?php print t('Total'); ?></th>
+       </tr>
+     </thead>
+     <tbody>
+       <?php if (!empty($records)): ?>
+         <?php foreach ($records as $record): ?>
+           <tr class="record--time-entries">
+             <th>
+               <p class="record--project"><?php print $record['project']?></p>
+               <p class="record--task"><?php print $record['task']?></p>
+             </th>
+             <?php foreach ($record['time_entries'] as $entry): ?>
+               <th><?php print $entry; ?></th>
              <?php endforeach; ?>
-             <th><?php print t('Total'); ?></th>
+             <th>
+               <?php print $record['total_hours_worked']; ?>
+             </th>
+             <th>
+               <?php print $record['remove_link_row'];?>
+             </th>
            </tr>
-         </thead>
-         <tbody>
-           <?php foreach ($records as $record): ?>
-             <tr class="record--time-entries">
-               <th>
-                 <p class="record--project"><?php print $record['project']?></p>
-                 <p class="record--task"><?php print $record['task']?></p>
-               </th>
-               <?php foreach ($record['time_entries'] as $entry): ?>
-                 <th><?php print $entry; ?></th>
-               <?php endforeach; ?>
-               <th>
-                 <?php print $record['total_hours_worked']; ?>
-               </th>
-               <th>
-                 <?php print $record['remove_link_row'];?>
-               </th>
-             </tr>
-           <?php endforeach; ?>
-         <tbody>
-       </table>
-     <?php endif; ?>
+         <?php endforeach; ?>
+       <?php endif; ?>
+     <tbody>
+   </table>
    <?php print $add_row_link; ?>
  </main>
